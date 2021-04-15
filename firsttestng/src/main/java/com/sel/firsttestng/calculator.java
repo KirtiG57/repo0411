936f6baab05e9516309cfe7e8e46ed732d1cd3a9
KirtiG57 <74084220+KirtiG57@users.noreply.github.com>
2021-04-14 21:38:40 -0500

    Add files via upload
    initial version

---
+++ firsttestng/src/main/java/com/sel/firsttestng/calculator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sel.firsttestng;
+
+/**
+ *
+ * @author deepa
+ */
+public class calculator {
+    
+    public int add(int x, int y)
+    {
+    return x+y;
+    }
+    
+     
+    public int mul(int x, int y)
+    {
+    return x*y;
+    }
+    
+}
